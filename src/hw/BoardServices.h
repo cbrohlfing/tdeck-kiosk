560ff808963767a440f07e90257fe0e3b6338534
Chris <cbrohlfing@gmail.com>
2026-01-05 20:56:49 -0600

    UI: add UiInput service hook

--- src/hw/BoardServices.h
+++ src/hw/BoardServices.h
@@ -2,17 +2,17 @@
 
 class Display;
 class Input;
+class UiInput;
 
 #if defined(HW_HELTEC_V3)
 class BatteryMonitor;
 class PowerButtonHeltecV3;
 #endif
 
-// Simple POD-style container for "what the board provides".
-// No constructor on purpose -> stays an aggregate and plays nicely with firmware.
 struct BoardServices {
   Display* display = nullptr;
   Input* input = nullptr;
+  UiInput* uiInput = nullptr;
 
 #if defined(HW_HELTEC_V3)
   BatteryMonitor* battery = nullptr;
